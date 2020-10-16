f7cf0139366e9921ab2214de9f74c95d42f022b6
Alexey Vitvor <vitvor01@gmail.com>
2020-10-16 09:12:35 +0300

    Create MVC architecture

---
+++ Models/src/main/java/Models/Contact.java
@@ -0,0 +1,27 @@
+package Models;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="contacts")
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+
+    private String PhoneNumber;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="User_Id")
+    private  User user;
+
+    @ManyToMany
+    @JoinTable(
+            name = "userscontacts",
+            joinColumns = @JoinColumn(name = "Contact_Id"),
+            inverseJoinColumns = @JoinColumn(name = "User_Id")
+    )
+    private Set<User> userSet;
+}
---
+++ Models/src/main/java/Models/User.java
@@ -0,0 +1,32 @@
+package Models;
+
+import com.sun.istack.Nullable;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+    @Column(name = "FirstName")
+    private String FirstName;
+    private String SecondName;
+    @Nullable
+    private String Patronymic;
+
+    @OneToOne(mappedBy = "user")
+    private Contact UserContact;
+
+    @ManyToMany
+    @JoinTable(
+            name="userscontacts",
+            joinColumns = @JoinColumn(name = "User_Id"),
+            inverseJoinColumns = @JoinColumn(name = "Contact_Id")
+    )
+    Set<Contact> contactSet;
+
+
+}
