c0ffe05e5251cf508b1ea377a0b588225697cb8d
Alexey Vitvor <vitvor01@gmail.com>
2020-10-21 09:11:09 +0300

    Already finish architecture

--- 2_8/src/main/java/com/aleks/_8/controllers/UserController.java
+++ 2_8/src/main/java/com/aleks/_8/controllers/UserController.java
@@ -1,14 +1,33 @@
 package com.aleks._8.controllers;
 
+import FrontModels.FrontUserForLogging;
+import Services.LoginService;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
 public class UserController {
+    private LoginService loginService;
+
     Logger logger = LoggerFactory.getLogger(UserController.class);
 
+    @Autowired
+    public UserController(LoginService service)
+    {
+        loginService=service;
+    }
+
+    @RequestMapping(value = {"/register"}, method = RequestMethod.POST)
+    public ModelAndView Register(FrontUserForLogging userForLogging)
+    {
+        ModelAndView modelAndView=new ModelAndView();
+        return modelAndView;
+    }
 }
---
+++ FrontModels/src/main/java/FrontModels/FrontUserForLogging.java
@@ -0,0 +1,4 @@
+package FrontModels;
+
+public class FrontUserForLogging {
+}
