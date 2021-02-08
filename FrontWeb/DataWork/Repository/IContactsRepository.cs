b96ae9d54a45ab20d51224d73db65b832e999889
Alexey Vitvor <vitvor01@gmail.com>
2021-02-08 13:15:00 +0300

    devServer: Add UOW and Repos

---
+++ FrontWeb/DataWork/Repository/IContactsRepository.cs
@@ -0,0 +1,19 @@
+﻿using Library.Context.Models;
+using System;
+using System.Collections.Generic;
+
+namespace DataWork.Repository
+{
+    public interface IContactsRepository : IDisposable
+    {
+        void RemoveRange(IEnumerable<Contact> contacts);
+
+        void Remove(Func<Contact, bool> predicate);
+
+        void Update(Contact contact);
+
+        Contact Get(Func<Contact, bool> predicate);
+
+        void Save();
+    }
+}
